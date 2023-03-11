89e555894b06db856c2003b5a1f4ad2c9ac7e3b6
mikeborodin <mikeborodin.dev@gmail.com>
2023-03-11 15:12:13 +0100

    version 0.0.9

--- Formula/terrun.rb
+++ Formula/terrun.rb
@@ -2,10 +2,10 @@
 class Terrun < Formula
   desc "terrun - efficient terminal runner"
   homepage "https://github.com/mikeborodin/terrun"
-  url "https://github.com/mikeborodin/terrun/releases/download/0.0.8/terrun.tar.gz"
-  sha256 "901206ae6650162516203a91fe283bca78bb6b6f7fe135d215d5116d5bcb2a00"
+  url "https://github.com/mikeborodin/terrun/releases/download/0.0.9/terrun.tar.gz"
+  sha256 "58ebc04cd53e69c8c5466997aae1b5596c3d94d9c8570a3299777aea6014f1bf"
   license "MIT"
-  version "0.0.8"
+  version "0.0.9"
 
   def install
     bin.install "terrun"
